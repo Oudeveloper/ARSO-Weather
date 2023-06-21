32047e9adc3109edaf3ffe3b423a929061830e84
root <root@DESKTOP-R801D1H.localdomain>
2023-06-21 19:32:57 +0200

    write data to sql

--- parse_xml.py
+++ parse_xml.py
@@ -6,9 +6,14 @@ import os
 from sql_queries import *
 import logging
 from ETL import log_etl
-
+from  datetime import datetime
 
 def get_credentials():
+    """read credentials of the database from credentials.txt file:
+    host
+    user
+    password
+    """
     try:
         with open('credentials.txt', 'r') as file:
             lines = file.readlines()
@@ -18,9 +23,9 @@ def get_credentials():
         logging.error(e)
         exit()
 
-#Create database and table
 @log_etl
 def create_database_and_table():
+    """Create database and table if not exists"""
     connection = None
     try:
         host, user, password = get_credentials()
@@ -46,7 +51,8 @@ def create_database_and_table():
             connection.close()
 @log_etl
 def insert_city_data():
-    #read all files from tmp folder and make a list of the name without extension
+    """read all files from tmp folder and make a list of the name without extension
+    check if city already exists in the table, if not insert it"""
     connection = None
     try:
         host, user, password = get_credentials()
@@ -71,9 +77,9 @@ def insert_city_data():
                 print(f"City {city} inserted successfully")
                 logging.info(f"City {city} inserted successfully")
             connection.commit()
+            print("Data inserted successfully")
+            logging.info('Data inserted successfully')
 
-        print("Data inserted successfully")
-        logging.info('Data inserted successfully')
     except Error as e:
         print(f"The error '{e}' occurred")
         logging.error(e)
@@ -82,23 +88,90 @@ def insert_city_data():
             cursor.close()
             connection.close()
 
+def find_city_id(city):
+    """find city id from the table cities based on city name"""
+    connection = None
+    try:
+        host, user, password = get_credentials()
+        connection = mysql.connector.connect(
+            host=host,
+            user=user,
+            password=password
+        )
+        if connection.is_connected():
+            cursor = connection.cursor()
+            cursor.execute(f"USE {database_name}")
+            cursor.execute(data_cityId_select, (city,))
+            city_id = cursor.fetchone()[0]
+            return city_id
+    except Error as e:
+        print(f"The error '{e}' occurred")
+        logging.error(e)
+    finally:
+        if connection is not None and connection.is_connected():
+            cursor.close()
+            connection.close()
+
+@log_etl
+def xml_parser(file):
+    """parse xml file and return a dataframe with the following columns:
+    datetime, temperature, city_id, humidity, wind_speed_km, pressure,
+    precipitation, solar_radiation, diffuse_solar_radiation"""
+
+    df = pd.read_xml(f'tmp/{file}', xpath="//metData")
+    city_name = file.split('.')[0]
+    df = df[['tsValid_issued_UTC', 't','rh','ff_val_kmh','p','tp_1h_acc','gSunRad','diffSunRad']]
+    #convert datetime so it can be inserted into the database
+    df['tsValid_issued_UTC'] = pd.to_datetime(df['tsValid_issued_UTC'], dayfirst=True)
+    df['tsValid_issued_UTC'] = df['tsValid_issued_UTC'].dt.strftime('%Y-%m-%d %H:%M:%S')
+    df['city_id'] = find_city_id(city_name)
+    df = df.rename(columns={'tsValid_issued_UTC': 'datetime',
+                            't': 'temperature',
+                            'city_id': 'city_id',
+                            'rh': 'humidity',
+                            'ff_val_kmh': 'wind_speed_km',
+                            'p': 'pressure',
+                            'tp_1h_acc': 'precipitation',
+                            'gSunRad': 'solar_radiation',
+                            'diffSunRad': 'diffuse_solar_radiation'
+                            })
+    logging.info(f'{file} parsed successfully')
+    return df
+
+@log_etl
+def insert_weather_data():
+    """read all files from tmp folder and insert data into weather table """
+    for file in os.listdir('tmp'):
+        if not file.endswith('.xml'):           #check if file is xml
+            continue
+        df=xml_parser(file)
+
+        host, user, password = get_credentials()
+        engine = create_engine(f'mysql+mysqlconnector://{user}:{password}@{host}/{database_name}', echo=False)
+        try:
+            df.to_sql(name='weather', con=engine, if_exists='append', index=False)
+            print(f'{file} inserted successfully')
+            logging.info(f'{file} inserted successfully')
+        except Exception as e:
+            if 'Duplicate entry' in str(e):
+                print(f'duplicate entry in {file} passed')
+                logging.info(f'duplicate entry in {file}')
+                continue
+            else:
+                print(e)
+                logging.error(e)
+        finally:
+            engine.dispose()
+    logging.info('weather table updated successfully')
+
 def main():
     if not os.path.exists('tmp'): #check if tmp folder exists
         print('tmp folder does not exist')
+        logging.error('tmp folder does not exist')
         exit()
-    df = pd.read_xml('tmp/BABNO-POL.xml', xpath="//metData")
-    print(df['tsValid_issued_UTC'].head())
     create_database_and_table()
     insert_city_data()
+    insert_weather_data()
 
 if __name__ == '__main__':
     main()
-
-
-#convert df to sqllite table even it does not exist
-# engine = create_engine('sqlite:///tmp/BABNO-POL.db', echo=False)
-# sqlite_connection = engine.connect()
-# sqlite_table = "BABNO-POL"
-# df.to_sql(sqlite_table, sqlite_connection, if_exists='fail')
-# sqlite_connection.close()
-
--- sql_queries.py
+++ sql_queries.py
@@ -1,24 +1,35 @@
 
 #Create database
-database_name = "WeatherData70"
+database_name = "WeatherData180"
 database_create =f"CREATE DATABASE IF NOT EXISTS {database_name};"""
 
 #Create table
 table_city_create = """CREATE TABLE IF NOT EXISTS cities (
-    id INT AUTO_INCREMENT PRIMARY KEY, city_name VARCHAR(255) NOT NULL unique);"""
+    id INT AUTO_INCREMENT PRIMARY KEY, 
+    city_name VARCHAR(255) NOT NULL unique);"""
 
 table_weather_create = """CREATE TABLE weather (
     id INT AUTO_INCREMENT,
     datetime DATETIME,
     city_id INT,
     temperature DECIMAL(5,2),
+    humidity smallint,
+    pressure smallint,
+    wind_speed_km smallint,
+    precipitation DECIMAL(5,2),
+    solar_radiation smallint,
+    diffuse_solar_radiation smallint,
     PRIMARY KEY (id),
+    FOREIGN KEY (city_id) REFERENCES cities(id),
     UNIQUE KEY (city_id, datetime)
     );"""
 
 #Insert data
 data_city_insert = """INSERT INTO cities (city_name) VALUES (%s);"""
-data_weather_insert = """INSERT INTO weather (datetime, city_id, temperature)
+data_weather_insert = """INSERT INTO weather (city_id, datetime,  temperature)
     VALUES (%s, %s, %s);"""
+
 #Select data
 data_city_select = """SELECT * FROM cities WHERE city_name = %s;"""
+data_cityId_select = """SELECT id FROM cities WHERE city_name = %s;"""
+
